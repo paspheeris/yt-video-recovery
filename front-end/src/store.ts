e27f4d7d883a2fa14f6710d6b9046155d1636b08
paspheeris <pa.spheeris@gmail.com>
2017-11-07 10:02:59 -0800

    Added localStorage util and hooks in store to save auth data on reload

--- front-end/src/store.ts
+++ front-end/src/store.ts
@@ -4,11 +4,18 @@ import * as Redux from 'redux';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import rootReducer from './reducers/root';
 import thunk from 'redux-thunk';
+import {loadState, saveState} from './utils/localStorage';
+
+// If there is data saved under 'auth' in localStorage, use it
+// in initialising the redux store
+const lsSavedAuth = loadState('auth');
 
 interface DefaultState {
-
+    auth?: any;
 }
-const defaultState = {};
+const defaultState= {
+    auth: lsSavedAuth ? lsSavedAuth : undefined
+};
 
 const enhancers = compose(
   applyMiddleware(thunk, reduxImmutableStateInvariant()),
@@ -16,6 +23,14 @@ const enhancers = compose(
 );
 
 const store: Redux.Store<DefaultState> = createStore(rootReducer, defaultState, enhancers);
+// Save the auth to localStorage on any state change
+store.subscribe(() => {
+  // This stores state on every state change, so it should be debounced
+  // if we ever add any actions to the app that fire rapidly and
+  // update the store quickly
+    saveState('auth', store.getState().auth);
+})
+
 if ((module as any).hot) {
   (module as any).hot.accept('./reducers/root', () => {
     const nextRootReducer = require('./reducers/root').default;
