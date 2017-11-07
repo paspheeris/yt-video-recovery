5ab19e3c0581fb65a5f4169f62c21ce7bbe84794
paspheeris <pa.spheeris@gmail.com>
2017-11-07 10:05:27 -0800

    Fix the expires_at calculation in the auth reducer

--- front-end/src/reducers/auth.ts
+++ front-end/src/reducers/auth.ts
@@ -18,7 +18,8 @@ interface InitState {
 function users(state: InitState = InitState, action: AuthActionInterface): InitState {
   switch (action.type) {
     case typeKeys.PARSE_HASH:
-      console.log(parseAuthHash(action.payload.hash));
+      console.log(action.payload);
+      //console.log(parseAuthHash(action.payload.hash));
       const { rawHash, access_token, token_type, expires_in, scope } = parseAuthHash(action.payload.hash);
       return {
         ...state,
@@ -26,7 +27,7 @@ function users(state: InitState = InitState, action: AuthActionInterface): InitS
         access_token,
         token_type,
         expires_in,
-        expires_at: expires_in ? expires_in + action.payload.timeReceived : action.payload.timeReceived,
+          expires_at: expires_in + action.payload.timeReceived,
         scope
       };
     default:
