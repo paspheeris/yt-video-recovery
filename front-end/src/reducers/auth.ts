c6c8ae7def351d3d55ca2c2793b4d494b860c802
paspheeris <pa.spheeris@gmail.com>
2017-11-07 20:30:45 -0800

    Fix auth reducer.

--- front-end/src/reducers/auth.ts
+++ front-end/src/reducers/auth.ts
@@ -8,30 +8,30 @@ const InitState = {
 
 };
 interface InitState {
-  rawHash?: string;
-  access_token?: string;
-  token_type?: string;
-  expires_in?: number;
-  expires_at?: number;
-  scope?: string;
+    rawHash?: string;
+    access_token?: string;
+    token_type?: string;
+    expires_in?: number;
+    expires_at?: number;
+    scope?: string;
 }
 function users(state: InitState = InitState, action: AuthActionInterface): InitState {
-  switch (action.type) {
-    case typeKeys.PARSE_HASH:
-      console.log(action.payload);
-      //console.log(parseAuthHash(action.payload.hash));
-      const { rawHash, access_token, token_type, expires_in, scope } = parseAuthHash(action.payload.hash);
-      return {
-        ...state,
-        rawHash,
-        access_token,
-        token_type,
-        expires_in,
-          expires_at: expires_in + action.payload.timeReceived,
-        scope
-      };
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case typeKeys.PARSE_HASH:
+            console.log(action.payload);
+            //console.log(parseAuthHash(action.payload.hash));
+            const { rawHash, access_token, token_type, expires_in, scope } = parseAuthHash(action.payload.hash);
+            return {
+                ...state,
+                rawHash,
+                access_token,
+                token_type,
+                expires_in,
+                expires_at: expires_in + action.payload.timeReceived,
+                scope
+            };
+        default:
+            return state;
+    }
 }
 export default users;
--- front-end/src/utils/auth.ts
+++ front-end/src/utils/auth.ts
@@ -3,65 +3,63 @@ const redirectURI: string = 'https://localhost:3000/profile';
 const clientID: string = '473384173845-k0go9oir9g6qsto4tmugfl8l5fcgvpc3.apps.googleusercontent.com';
 
 export interface AuthHash {
-  rawHash: string;
-  state: string;
-  access_token: string;
+    rawHash: string;
+    state: string;
+    access_token: string;
     token_type: string;
     expires_in: number;
     scope: string;
 }
 
 export function parseAuthHash(hash: string): AuthHash {
-  // const raw = action.payload;
-  // console.log(raw);
-  // console.log(raw.split('&'));
-  const authHash = hash.split('&').reduce((accum: AuthHash, fieldStr: string): AuthHash => {
-    let [key, value] = fieldStr.split('=');
-    // remove the leading '#' from '#state'
-    if (key[0] === '#') { key = key.slice(1); }
-      if(key === 'expires_in') {
-       const expiresInt = parseInt(value, 10);
-          accum[key] = expiresInt;
-      } else {
-    accum[key] = value;
-      }
-    return accum;
-  }, {} as AuthHash);
-  return authHash;
+    // const raw = action.payload;
+    const authHash = hash.split('&').reduce((accum: AuthHash, fieldStr: string): AuthHash => {
+        let [key, value] = fieldStr.split('=');
+        // remove the leading '#' from '#state'
+        if (key[0] === '#') { key = key.slice(1); }
+        if (key === 'expires_in') {
+            const expiresInt = parseInt(value, 10);
+            accum[key] = expiresInt;
+        } else {
+            accum[key] = value;
+        }
+        return accum;
+    }, {} as AuthHash);
+    return authHash;
 }
 /* Google's oauth flow doesn't allow CORS, so a form must be created and appended to the page.
    T oauthSignIn() initiaites a redirect, and after permission granting, redirects to redirectURI.
    The JWT is put in the url of the redirectURI, where it can then be parsed.
    src: https://developers.google.com/youtube/v3/guides/auth/client-side-web-apps */
 export function oauthSignIn() {
-  // Google's OAuth 2.0 endpoint for requesting an access token
-  var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+    // Google's OAuth 2.0 endpoint for requesting an access token
+    var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
 
-  // Create <form> element to submit parameters to OAuth 2.0 endpoint.
-  var form = document.createElement('form');
-  form.setAttribute('method', 'GET'); // Send as a GET request.
-  form.setAttribute('action', oauth2Endpoint);
+    // Create <form> element to submit parameters to OAuth 2.0 endpoint.
+    var form = document.createElement('form');
+    form.setAttribute('method', 'GET'); // Send as a GET request.
+    form.setAttribute('action', oauth2Endpoint);
 
-  // Parameters to pass to OAuth 2.0 endpoint.
-  var params = {
-    'client_id': `${clientID}`,
-    'redirect_uri': `${redirectURI}`,
-    'response_type': 'token',
-    'scope': `${scope}`,
-    'include_granted_scopes': 'true',
-    'state': 'pass-through value'
-  };
+    // Parameters to pass to OAuth 2.0 endpoint.
+    var params = {
+        'client_id': `${clientID}`,
+        'redirect_uri': `${redirectURI}`,
+        'response_type': 'token',
+        'scope': `${scope}`,
+        'include_granted_scopes': 'true',
+        'state': 'pass-through value'
+    };
 
-  // Add form parameters as hidden input values.
-  for (var p in params) {
-    var input = document.createElement('input');
-    input.setAttribute('type', 'hidden');
-    input.setAttribute('name', p);
-    input.setAttribute('value', params[p]);
-    form.appendChild(input);
-  }
+    // Add form parameters as hidden input values.
+    for (var p in params) {
+        var input = document.createElement('input');
+        input.setAttribute('type', 'hidden');
+        input.setAttribute('name', p);
+        input.setAttribute('value', params[p]);
+        form.appendChild(input);
+    }
 
-  // Add form to page and submit it to open the OAuth 2.0 endpoint.
-  document.body.appendChild(form);
-  form.submit();
+    // Add form to page and submit it to open the OAuth 2.0 endpoint.
+    document.body.appendChild(form);
+    form.submit();
 }
