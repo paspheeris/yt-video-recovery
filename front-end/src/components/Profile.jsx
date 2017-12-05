c7dbfea8ede5eef785bc3f4c9b86fe4a21914a45
paspheeris <pa.spheeris@gmail.com>
2017-12-05 11:29:26 -0800

    Simple call to the google api now working (all auth working) in Profile.jsx.

--- front-end/src/components/Profile.jsx
+++ front-end/src/components/Profile.jsx
@@ -7,19 +7,35 @@ import { parseHash } from '.././actions/auth';
 class Profile extends React.Component {
     componentDidMount() {
         this.props.parseHash(this.props.hash, Date.now());
+        this.props.history.push('/profile');
     }
     render() {
         return (
             <div>
                 hello from the Profile
-      </div>
+                wtf
+                <button onClick={this.handleGoogleCall}>
+                    Calling Google Api
+                </button>
+            </div>
         );
+    }
+    handleGoogleCall = () => {
+        /* fetch(`https://www.googleapis.com/youtube/v3/channels?access_token=ya29.Gl0ZBX8XWGuKKnQ-RayKwkM806-Q1vgHPdvdE_czat3R0IQ0--K9BL6zrZ_XIii2SWqMmoCDyvb5NN9vi96twDaPGdz-0eEbCBftc4h46ixW7CzmHOv3EHhiZC3baww&part=snippet&mine=true`)*/
+ 
+
+        fetch(`https://www.googleapis.com/youtube/v3/channels?access_token=${this.props.access_token}&part=snippet&mine=true`)
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error));
     }
 }
 
 function mapStateToProps(state, ownProps) {
     return {
         hash: ownProps.location.hash,
+        history: ownProps.history,
+        access_token: state.auth.access_token
     };
 }
 function mapDispatchToProps(dispatch) {
