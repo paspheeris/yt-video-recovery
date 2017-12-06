dd5cae64f5c2a6f3a1c5695efbfdd95955613446
paspheeris <pa.spheeris@gmail.com>
2017-12-06 12:19:11 -0800

    Profile component cleaned up and showing titles of playlists correctly.

--- front-end/src/components/Profile.jsx
+++ front-end/src/components/Profile.jsx
@@ -18,33 +18,19 @@ class Profile extends React.Component {
         return (
             <div>
                 hello from the Profile
-                wtf
-                <button onClick={this.handleGoogleCall}>
-                    Calling Google Api
-                </button>
-                <button onClick={this.getPlaylistsTitles}>
-                    Get playlists titles
-                </button>
-                <button onClick={this.getPlaylistsTitlesPromise}>
-                    get playlists ttiels promise
-            </button>
+                <div>
+                    Your Playlists
+                    {this.props.summaries && this.props.summaries.map(summary => {
+                         return <div>{summary.title}</div>
+                    })}
+                </div>
+               <button onClick={this.getPlaylists}>
+                 get playlists ttiels promise
+               </button>
             </div>
         );
     }
-    handleGoogleCall = () => {
-        fetch(`https://www.googleapis.com/youtube/v3/channels?access_token=${this.props.access_token}&part=snippet&mine=true`)
-        .then(res => res.json())
-        .then(data => console.log(data))
-        .catch(error => console.log(error));
-    }
-    getPlaylistsTitles = () => {
-        fetch(`https://www.googleapis.com/youtube/v3/playlists?part=snippet&maxResults=50&mine=true&access_token=${this.props.access_token}`)
-          .then(res => res.json())
-          .then(data => console.log(data))
-          .catch(error => console.log(error));
-
-    }
-    getPlaylistsTitlesPromise = () => {
+    getPlaylists = () => {
         this.props.getPlaylists();
     }
 }
@@ -53,7 +39,8 @@ function mapStateToProps(state, ownProps) {
     return {
         hash: ownProps.location.hash,
         history: ownProps.history,
-        access_token: state.auth.access_token
+        access_token: state.auth.access_token,
+        summaries: state.playlists.summaries
     };
 }
 function mapDispatchToProps(dispatch) {
