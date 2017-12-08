46b490395de256375f512173e8a3e3ba202d2862
paspheeris <pa.spheeris@gmail.com>
2017-12-08 10:38:44 -0800

    Started on backend. Basic validation of token and fetching a playlist given a token working.

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+const fetch = require('node-fetch');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+const tempToken = `ya29.Gl0cBZ_FePpLi7v_UtAgYi0T9zKsCkLMjtwR-wyPtFz_EwbF1zPs-FabliqOT4l9ZT7S7ABfib0wXE15O939l-8Ep2zDQ-wsGg_sdzgfqisSd41dpV83IwmmL99En5E`;
+router.get('/test', function(req, res, next) {
+    fetch(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=PLrkcX2uLOH-ivyYR9PYi_gG4Gwzk26R75&access_token=${tempToken}`)
+    
+        .then(data => data.json())
+        .then(data => {
+            // console.log(data);
+            res.json(data);
+        })
+        .catch(error => console.log(error));
+    // res.json()
+});
+router.get('/validateToken', function(req, res, next) {
+    fetch(`https://www.googleapis.com/oauth2/v3/tokeninfo?access_token=${tempToken}`)
+        .then(data => data.json())
+        .then(data => {
+            // console.log(data);
+            res.json(data);
+        })
+        .catch(error => console.log(error));
 });
 
 module.exports = router;
