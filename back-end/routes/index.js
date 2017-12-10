4a810d6c55634a69983ee43d11513c9726067b10
paspheeris <pa.spheeris@gmail.com>
2017-12-10 12:51:14 -0800

    Added ability to start the express server in HTTPS with a self-signed cert for use with the https page on the front end.

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var router = express.Router();
 const fetch = require('node-fetch');
+const {server} = require('.././bin/www');
+var io = require('socket.io')(server);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 const tempToken = `ya29.Gl0cBZ_FePpLi7v_UtAgYi0T9zKsCkLMjtwR-wyPtFz_EwbF1zPs-FabliqOT4l9ZT7S7ABfib0wXE15O939l-8Ep2zDQ-wsGg_sdzgfqisSd41dpV83IwmmL99En5E`;
+// router.get('/home', function(req, res, next) {
+//     res.sendFile('index');
+// });
 router.get('/test', function(req, res, next) {
     fetch(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=PLrkcX2uLOH-ivyYR9PYi_gG4Gwzk26R75&access_token=${tempToken}`)
     
@@ -26,6 +31,9 @@ router.get('/validateToken', function(req, res, next) {
             res.json(data);
         })
         .catch(error => console.log(error));
+});
+io.on('connection', function(socket){
+    console.log('a user connected');
 });
 
 module.exports = router;
