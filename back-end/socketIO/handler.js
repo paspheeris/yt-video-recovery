38e09b498762bccdbc7b8328a25887a1333ad49d
paspheeris <pa.spheeris@gmail.com>
2017-12-17 12:09:01 -0800

    Broke socket handling functionality out into it's own file. Currently has an issue with creating a new client instance on every connection.

---
+++ back-end/socketIO/handler.js
@@ -0,0 +1,32 @@
+const fetch = require('node-fetch');
+const { validateAccessToken,
+			  parseValidationRes,
+			  getPlaylists,
+			  parsePlaylistRes } = require('../utils/youtube');
+
+function socketHandler(client) {
+  console.log('connect in the wwww');
+  client.on('test', (d) => {
+    console.log(d);
+    client.emit('testDataReceived', d);
+  });
+  client.on('accessToken', token => {
+    // Validate token, then...
+    validateAccessToken(token)
+    // ...parse res into a form to use
+      .then(validationRes => parseValidationRes(validationRes))
+    // .then(userInfo => console.log(userInfo))
+    // ...check the db for user info here and send if found
+    // ...get playlist data for the user
+      .then(_ => getPlaylists(token))
+      .then(playlistRes => parsePlaylistRes(playlistRes))
+      .then(playlistObjs => console.log(playlistObjs))
+      .catch(error => console.log(error));
+  });
+}
+
+
+
+module.exports = {
+	socketHandler
+};
---
+++ back-end/utils/youtube.js
@@ -0,0 +1,57 @@
+const fetch = require('node-fetch');
+
+const YT_API_ROOT = `https://www.googleapis.com/youtube/v3`;
+const  User  = require('.././models/User');
+// YtUser.save
+const testUser = new User({email: 'hurr', expiresAt: 123});
+testUser.save()
+    .then(saved => {
+        console.log('successfully saved:', saved);
+    })
+    .catch(error => {
+        console.log(error)
+    });
+
+function validateAccessToken(token) {
+    return fetch(`https://www.googleapis.com/oauth2/v3/tokeninfo?access_token=${token}`)
+        .then(data => data.json())
+        .catch(error => console.log(error));
+};
+function parseValidationRes(validationRes) {
+    // console.log('validationRes in parseValidationRes', validationRes);
+    if(validationRes.error || validationRes.error_description) {
+        return Promise.reject({
+                error_description: 'Invalid or expired token',
+                error_text: validationRes.error_description,
+                error_location: 'parseValidationRes'
+            });
+    } else return ({
+        email: validationRes.email,
+        expiresAt: Date.now() + parseInt(validationRes.expires_in, 10)
+    });
+}
+function getPlaylists(accessToken) {
+    return fetch(`${YT_API_ROOT}/playlists?part=snippet&maxResults=50&mine=true&access_token=${accessToken}`)
+        .then(data => data.json())
+        .catch(error => console.log(error));
+}
+function parsePlaylistRes(playlistRes) {
+    // console.log('playlistRes in parsePlaylistRes:', playlistRes);
+    return playlistRes.items.map(playlist => {
+        // console.log(playlist.snippet);
+        return {
+            title: playlist.snippet.title,
+            id: playlist.id,
+            thumbnail: playlist.snippet.thumbnails.medium,
+            created: playlist.snippet.publishedAt
+        };
+    });
+
+}
+
+module.exports =  {
+	validateAccessToken,
+	parseValidationRes,
+	getPlaylists,
+	parsePlaylistRes
+};
