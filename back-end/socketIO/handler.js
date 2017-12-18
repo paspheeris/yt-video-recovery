a44e4c5c96ec180b239e3c9bf831da66aafd8975
paspheeris <pa.spheeris@gmail.com>
2017-12-18 10:49:00 -0800

    Working on saving playlists data for users found in the DB.

--- back-end/socketIO/handler.js
+++ back-end/socketIO/handler.js
@@ -3,6 +3,9 @@ const { validateAccessToken,
 			  parseValidationRes,
 			  getPlaylists,
 			  parsePlaylistRes } = require('../utils/youtube');
+const { saveTest,
+			  saveUser,
+				savePlaylists} = require('../utils/mongodb.js');
 
 function socketHandler(client) {
   console.log('connect in the wwww');
@@ -11,17 +14,32 @@ function socketHandler(client) {
     client.emit('testDataReceived', d);
   });
   client.on('accessToken', token => {
+		let userEmail;
     // Validate token, then...
     validateAccessToken(token)
     // ...parse res into a form to use
       .then(validationRes => parseValidationRes(validationRes))
-    // .then(userInfo => console.log(userInfo))
-    // ...check the db for user info here and send if found
-			.then(userObj => console.log(userObj))
+    // ...save the user in DB if they're not already there
+			.then(userObj => {
+				// Save userEmail in the local scope so that it can be used through
+				// the rest of the promise chain
+				userEmail = userObj.email;
+				return saveUser(userObj);
+			})
+			.then(userData => {
+				// Send saved user data to the frontend, if found
+				// ie. send playlist titles and playlists that are already saved
+				if(userData !== null) {
+					// console.log('userData in promise chain:', userData);
+				}
+			})
     // ...get playlist data for the user
       .then(_ => getPlaylists(token))
       .then(playlistRes => parsePlaylistRes(playlistRes))
-      .then(playlistObjs => console.log(playlistObjs))
+      .then(playlistObjs => {
+				// Save any new playlists in the DB under the user
+				savePlaylists(userEmail, playlistObjs);
+			})
       .catch(error => console.log(error));
   });
 }
--- back-end/utils/mongodb.js
+++ back-end/utils/mongodb.js
@@ -1,11 +1,55 @@
-const  User  = require('.././models/User');
+const User  = require('.././models/User');
+// const Playlist = require('.././models/Playlist');
+
 // YtUser.save
-const testUser = new User({email: 'hurr', expiresAt: 123});
-testUser.save()
-  .then(saved => {
-    console.log('successfully saved:', saved);
-  })
-  .catch(error => {
-    console.log(error)
-  });
+function saveTest() {
+	const testUser = new User({email: 'hurr', expiresAt: 123});
+	testUser.save()
+		.then(saved => {
+			console.log('successfully saved:', saved);
+		})
+		.catch(error => {
+			console.log(error);
+		});
+}
+function saveUser(userObj) {
+	return User.findOne({ email: userObj.email })
+		.then(data => {
+			if (data === null) {
+				// User isn't saved in the DB yet, save them
+				console.log('Saving a new user in saveUser function');
+				const newUser = new User(userObj);
+				newUser.save();
+				return null;
+			} else {
+				// User was found in the DB, return their data
+				// console.log('User found in saveUser function:', data);
+				return data;
+			}
+		})
+		.catch(error => console.log(error));
+}
+function savePlaylists(userEmail, playlistObjs) {
+	console.log('playlistObjs in savePlaylists:', playlistObjs);
+
+    // .then(something => {
+    //   something.votesByChoice.push({ choiceName: req.body.choice, count: 1 });
+    //   something.allChoices.push(req.body.choice);
+    //   return something.save()
+    // })
+
+	
+	// const poll = Poll.findOneAndUpdate(
+  //   { _id: req.params._id, 'votesByChoice.choiceName': req.body.choice },
+  //   { $inc: { 'votesByChoice.$.count': 1 } }, {
+  //     new: true, //return the new Poll instead of the old one
+  //     runValidators: true
+  //   }).exec()
+	// 			.then(something => {
+}
+module.exports = {
+	saveTest,
+	saveUser,
+	savePlaylists
+};
 
